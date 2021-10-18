5cbd15054bdfd0ecc3af107bd4cc64e9a288616a
felixmaina <karani.maina2010@gmail.com>
2021-10-18 11:51:29 +0300

    add observable unsubscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
-
+import { Subscription }from 'rxjs'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,OnDestroy {
   title = 'git-search';
-  username:string = 'karanimaina'
+  username:string = 'karanimaina' 
   repositories: any[]= [];
+  mySubscription: any;
   constructor(private usersService:UsersService){} 
+  ngOnDestroy(): void {
+    this.mySubscription.unsubscribe();
+  }
   ngOnInit(): void {
     this.getPublicRepositories();
   }
@@ -18,11 +22,18 @@ export class AppComponent implements OnInit {
 
 
   getPublicRepositories(){
-    this.usersService.getUsers(this.username).subscribe((response)=>{
-      // console.log(response)
-      this.repositories= response
-    }
-  
 
-  
-    )}}
+    // this.usersService.getUsers(this.username).subscribe((response)=>{
+    //   // console.log(response)
+    //   this.repositories= response
+    // }
+  this.mySubscription.add(
+     this.usersService.getUsers(this.username).subscribe((response)=>{
+      // console.log(response)
+      this.repositories= response;
+      console.log(this.repositories.length);
+     })
+  )
+    }
+}
+    
