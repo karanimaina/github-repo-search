42578b1b5c5d72c3ecc6e5d1523d0a13d3073bae
felixmaina <karani.maina2010@gmail.com>
2021-10-21 00:17:05 +0300

    edit the navbar section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
-// import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router';
-// import { HomeComponent } from './home/home.component';
-// import { UserComponent } from './user/user.component';
-// import { ReposComponent } from './repos/repos.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { ReposComponent } from './repos/repos.component';
 
-// const routes: Routes = [
-//   {
-//     path: '',
-//     pathMatch: 'full',
-//     redirectTo: 'home'
-//   },
-//   { path: 'home', component: HomeComponent},
-//   { path: 'user', component: UserComponent},
-//   { path: 'repos', component: ReposComponent},
-// ];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  { path: 'home', component: HomeComponent},
+  { path: 'user', component: UserComponent},
+  { path: 'repos', component: ReposComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
